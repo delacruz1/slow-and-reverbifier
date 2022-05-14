6b9316bbcec293f67c4aa3f0fe57208b8a1fa26e
delacruz1 <jdelacr123@gmail.com>
2022-05-13 19:24:06 -0700

    initial commit

---
+++ slowAndReverb.py
@@ -0,0 +1,22 @@
+from pedalboard import Pedalboard, Reverb
+from pedalboard.io import AudioFile
+
+
+'''
+Function for slowing down and reverbing songs (.wav file format).
+'''
+def processAudio(filename, roomSize, sampleRate):
+    with AudioFile(filename, 'r') as f:
+        audio = f.read(f.frames)
+    board = Pedalboard([Reverb(room_size=roomSize)])
+    effected = board(audio, sampleRate)
+    splitFileName = filename.split("/")
+    fname = splitFileName[len(splitFileName) - 1]
+    with AudioFile(f'slowAndReverb_{fname}', 'w', sampleRate, effected.shape[0]) as out:
+        out.write(effected)
+
+
+def processMultipleAudioFiles(fileList, roomSize, sampleRate):
+    for f in fileList:
+        processAudio(f, roomSize, sampleRate)
+
