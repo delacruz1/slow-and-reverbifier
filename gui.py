fc5440877941e4a4053115da68d8cc76502a3747
delacruz1 <jdelacr123@gmail.com>
2022-05-16 18:55:59 -0700

    added additional buttons, labels, sliders + refactor + working program

--- gui.py
+++ gui.py
@@ -1,36 +1,117 @@
-import slowAndReverb
 import sys
-from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QVBoxLayout, QWidget, QSlider, QHBoxLayout
+import os
+from slowAndReverb import processAudio
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtWidgets import (
+    QApplication, 
+    QLabel, 
+    QPushButton, 
+    QMessageBox, 
+    QVBoxLayout, 
+    QWidget, 
+    QSlider, 
+    QHBoxLayout, 
+    QFileDialog,
+)
+
 
 class SlowAndReverbifier(QWidget):
     def __init__(self):
         super().__init__()
+        self._setUpInterface()
+    
+    def _setUpInterface(self):
+    
+        # Song Configuration Choices
+        self.speedAmounts = [
+                38000, 
+                39000, 
+                40000, 
+                41000,
+                42000, 
+                43000, 
+                44000, 
+                45000, 
+                46000, 
+                47000
+        ]
+        self.reverbAmounts = [
+            0.0, 
+            0.10, 
+            0.25, 
+            0.5
+        ]
+
         self.fileName = None
-        self.speed = None
-        self.roomSize = 0
+        self.speed = self.speedAmounts[0]
+        self.roomSize = self.reverbAmounts[0]
 
+        # Window Layout Setup
+        self.resize(600, 200)
+        center = QGuiApplication.screens()[0].geometry().center()
+        self.move(center - self.frameGeometry().center())
         self.setWindowTitle("Slow and Reverbifier")
-        self.setAcceptDrops(True)
-
-        self.button = QPushButton("Hello")
         layout = QVBoxLayout()
         self.setLayout(layout)
 
+        # Song Picker
+        self.fileLabel = QLabel(f'File Selected: {self.fileName}')
+        layout.addWidget(self.fileLabel)
+        self.fileButton= QPushButton("Choose a Song (in .wav format)")
+        self.fileButton.clicked.connect(self._getFile)
+        layout.addWidget(self.fileButton)
+
+        # Song Speed Picker
         speedBox = QHBoxLayout()
         speedLabel = QLabel("Song Speed: ")
-        self.speedSlider = QSlider(Qt.Horizontal, minimum=30000, maximum=40000)
-        self.speedSlider.setTickPosition(QSlider.TicksBelow)
-        self.speedSlider.setTickInterval(1000)
-        self.speedSlider.setPageStep(1000)
+        self.speedSlider = QSlider(Qt.Horizontal, minimum=0, maximum=len(self.speedAmounts)-1)
+        self.speedSlider.setTickPosition(QSlider.TicksBothSides)
+        self.speedSlider.setTickInterval(1)
+        self.speedSlider.setSingleStep(1)
         self.speedSlider.valueChanged.connect(self._speedChanged)
 
         speedBox.addWidget(speedLabel)
         speedBox.addWidget(self.speedSlider)
         layout.addLayout(speedBox)
 
+        # Reverb Room Size Picker
+        reverbBox = QHBoxLayout()
+        reverbLabel = QLabel("Reverb Amount: ")
+        self.reverbSlider = QSlider(Qt.Horizontal, minimum=0, maximum=len(self.reverbAmounts)-1)
+        self.reverbSlider.setTickPosition(QSlider.TicksBothSides)
+        self.reverbSlider.setTickInterval(1)
+        self.reverbSlider.setSingleStep(1)
+        self.reverbSlider.valueChanged.connect(self._reverbChanged)
+
+        reverbBox.addWidget(reverbLabel)
+        reverbBox.addWidget(self.reverbSlider)
+        layout.addLayout(reverbBox)
+
+        # Slow and Reverb Button
+        self.slowAndReverbButton = QPushButton("Slow and Reverbify!")
+        self.slowAndReverbButton.setEnabled(False)
+        self.slowAndReverbButton.clicked.connect(lambda: processAudio(self.fileName, self.roomSize, self.speed))
+        
+        if self.fileName:
+            self.slowAndReverbButton.setEnabled(True)
+        layout.addWidget(self.slowAndReverbButton)
+
+    # File Dialog Helper
+    def _getFile(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', './', "Wav file (*.wav)")
+        self.fileName = fname[0]
+        self.fileLabel.setText(f'File Selected: {os.path.basename(fname[0])}')
+        if fname:
+            self.slowAndReverbButton.setEnabled(True)
+
+    # Speed Changer Helper
     def _speedChanged(self, value):
-        print(value)
+        self.speed = self.speedAmounts[value]
+    
+    # Reverb Room Size Helper
+    def _reverbChanged(self, value):
+        self.roomSize = self.reverbAmounts[value]
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- slowAndReverb.py
+++ slowAndReverb.py
@@ -1,22 +1,21 @@
 from pedalboard import Pedalboard, Reverb
 from pedalboard.io import AudioFile
+import os
 
 
-'''
-Function for slowing down and reverbing songs (.wav file format).
-'''
+
+# Function for slowing down and reverbing songs (.wav file format).
+
 def processAudio(filename, roomSize, sampleRate):
     with AudioFile(filename, 'r') as f:
         audio = f.read(f.frames)
-    board = Pedalboard([Reverb(room_size=roomSize)])
+    board = Pedalboard([Reverb(roomSize)])
     effected = board(audio, sampleRate)
-    splitFileName = filename.split("/")
-    fname = splitFileName[len(splitFileName) - 1]
+    fname = os.path.basename(filename)
     with AudioFile(f'{roomSize}_{sampleRate}_slowAndReverb_{fname}', 'w', sampleRate, effected.shape[0]) as out:
         out.write(effected)
 
-
+# Function for handling multiple audio files
 def processMultipleAudioFiles(fileList, roomSize, sampleRate):
     for f in fileList:
         processAudio(f, roomSize, sampleRate)
-
